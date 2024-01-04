7452c6d6e29d471bcb32d83d17afa500331e71b4
Rafael Souza <rafa.nds13@gmail.com>
2024-01-04 16:45:04 -0300

    Tratando exceções e limpando a tela

--- src/main/java/com/rafael/App.java
+++ src/main/java/com/rafael/App.java
@@ -1,8 +1,10 @@
 package com.rafael;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.rafael.application.UI;
+import com.rafael.chess.ChessException;
 import com.rafael.chess.ChessMatch;
 import com.rafael.chess.ChessPiece;
 import com.rafael.chess.ChessPosition;
@@ -13,16 +15,27 @@ public class App
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         while(true){
-            UI.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Origem: ");
-            ChessPosition source = UI.readChessPosition(sc);
-
-            System.out.println();
-            System.out.print("Destino: ");
-            ChessPosition target = UI.readChessPosition(sc);
-
-            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Origem: ");
+                ChessPosition source = UI.readChessPosition(sc);
+    
+                System.out.println();
+                System.out.print("Destino: ");
+                ChessPosition target = UI.readChessPosition(sc);
+    
+                ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+            }catch(ChessException e){
+                System.out.println(e.getMessage());
+                System.out.println("Aperte enter para continuar...");
+                sc.nextLine();
+            }catch(InputMismatchException e){
+                System.out.println(e.getMessage());
+                System.out.println("Aperte enter para continuar...");
+                sc.nextLine();
+            }
         }
     }
 }
